39842782fef51acf59e675fbc379374fa76bbf89
IFFranciscoME <if.francisco.me@gmail.com>
2021-07-08 09:53:57 -0500

    project visualizations functions

---
+++ visualizations.py
@@ -0,0 +1,17 @@
+
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- MarketMaker-BackTest                                                                                -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- file: visualizations.py                                                                             -- #
+# -- Description: Functions for plots, tables and text visualizations for the project                    -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- Author: IFFranciscoME - if.francisco.me@gmail.com                                                   -- #
+# -- license: MIT License                                                                                -- #
+# -- Repository: https://github.com/IFFranciscoME/MarketMaker-BackTest                                   -- #
+# --------------------------------------------------------------------------------------------------------- #
+
+# -- Load base packages
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+from rich import inspect
